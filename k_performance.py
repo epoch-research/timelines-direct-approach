926237e0761565cce6ad99dbaa51dee5ff56a8db
Eirol <74545840+eirol@users.noreply.github.com>
2023-08-23 11:49:14 +0200

    fix k_performance bugs

--- k_performance.py
+++ k_performance.py
@@ -18,7 +18,7 @@ def k_performance(kl_divergence, slowdown=1, confidence=0.9):
 
 # Finds the optimal settings for parameters (N) and data (D) given a compute budget (C)
 def optimal_N_D(C, A=406.4, B=410.7, alpha=0.34, beta=0.28):
-    G = (alpha * A)/(beta * B)
+    G = (alpha * A)/(beta * B) ** (1 / (alpha + beta))
     a = beta/(alpha + beta)
     b = alpha/(alpha + beta)
     return (G*(C/6)**a, G**(-1)*(C/6)**b)
@@ -31,16 +31,18 @@ def computation_for_k_performance(k, slowdown=1):
     B = np.random.normal(410.7, 25)
     alpha = np.random.normal(0.34, 0.05)
     beta = np.random.normal(0.28, 0.05)
-    compute1 = 10
 
-    N_opt, D_opt = optimal_N_D(10**compute1)
-    kl_divergence = KL_divergence(N_opt, D_opt)
-    log10_k_perf1 = np.log10(k_performance(kl_divergence, slowdown))
-    compute2 = 30
-    N_opt, D_opt = optimal_N_D(10**compute2, A=A, B=B, alpha=alpha, beta=beta)
-    kl_divergence = KL_divergence(N_opt, D_opt, A=A, B=B, alpha=alpha, beta=beta)
-    log10_k_perf2 = np.log10(k_performance(kl_divergence, slowdown))
-    m = 20.0/(log10_k_perf2 - log10_k_perf1)
-    b = 10 - log10_k_perf1*m
+    compute_points = [10, 30]
+    log10_k_perf_points = []
 
-    return m*np.log10(k) + b
+    for compute in compute_points:
+        N_opt, D_opt = optimal_N_D(10**compute, A=A, B=B, alpha=alpha, beta=beta)
+        kl_divergence = KL_divergence(N_opt, D_opt, A=A, B=B, alpha=alpha, beta=beta)
+        log10_k_perf = np.log10(k_performance(kl_divergence, slowdown))
+        log10_k_perf_points.append(log10_k_perf)
+
+    m = np.diff(compute_points)/np.diff(log10_k_perf_points)
+    b = compute_points[0] - log10_k_perf_points[0]*m
+
+    compute = m*np.log10(k) + b
+    return compute
